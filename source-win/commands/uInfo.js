96b6514c645904bdcd85e40f733eabb95ad8a65d
BiIINye <34324304+BiIINye@users.noreply.github.com>
2021-04-03 23:12:41 -0700

    user info function!
    added new user info function that displays some info about a specified user when u type !!info and tag the user

--- source-win/commands/uInfo.js
+++ source-win/commands/uInfo.js
@@ -7,20 +7,65 @@ module.exports = {
     description: `asdf.`,
     execute(msg) {
 
-        var msgContent = msg.content.slice(6);
-
-        var userId = msg.mentions.users.first().id;
-
-        console.log(userId);
-
-        if (userId === undefined) {
-            console.log(`You must mention a user!`);
+        if (msg.guild === null) {
+            console.log('This command only works in a server.');
         }
         else {
-            const uInfoEmbed = new Discord.RichEmbed()
-                .setTitle()
-                .setDescription()
-        }
 
+            if (msg.mentions.users.first() === undefined) {
+                console.log(`You must mention a user!`);
+            }
+            else {
+
+                var userId = msg.mentions.users.first().id;
+
+                const member = msg.guild.member(userId);
+
+                var bot
+
+                if (member.user.bot === true) {
+                    bot = 'bot';
+                }
+                else {
+                    bot = 'user';
+                }
+
+                var nick;
+
+                var dIdLink = `https://discord.id/?prefill=` + userId;
+
+                var joinDate = member.joinedAt.toString();
+                joinDate = joinDate.slice(0, 15);
+
+                var createDate = member.user.createdAt.toString();
+                createDate = createDate.slice(0, 15);
+
+                var color = member.displayHexColor;
+
+                if (member.nickname === null) {
+                    nick = 'none';
+                }
+                else {
+                    nick = member.nickname;
+                }
+
+                if (msg.guild.member === null) {
+                    const foreignUser = new Discord.RichEmbed()
+                        .setDescription('This user is not present in this server. However you can still search for the user with [discord.id](' + dIdLink + ')')
+
+                    msg.channel.send(foreignUser);
+                }
+                else {
+                    const uInfoEmbed = new Discord.RichEmbed()
+                        .setAuthor(member.user.tag)
+                        .setDescription('\n**Joined: **`' + joinDate + '`\n\n**Created: **`' + createDate + '`\n\n**Nickname: **`' + nick + '`\n\n**Bot status: **`' + bot + '`\n\n[Search user on discord.id](' + dIdLink + ')')
+                        .setThumbnail(member.user.displayAvatarURL)
+                        .setColor(color)
+
+                    msg.delete();
+                    msg.channel.send(uInfoEmbed);
+                }
+            }
+        }
     },
 };
